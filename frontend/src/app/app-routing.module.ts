2125d9b7e8ee8471f6577d23c7b9bcc907405b1f
schw-proj <sven.schweigert@fau.de>
2020-05-26 20:02:32 +0200

    added routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { LoginComponent} from './components/login/login.component';
-
+import { TodoComponent } from './components/todo/todo.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { MoneyShopComponent } from './components/money-shop/money-shop.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'login', pathMatch:'full'},
   {path:'login', component: LoginComponent},
   {path:'home',component: MainNavComponent,
     children:[
-
+      {path:'todo', component: TodoComponent},
+      {path:'friends', component: FriendsComponent},
+      {path:'shop', component: MoneyShopComponent},
   ]},
   {path:'**',redirectTo: 'login', pathMatch:'full'},
 ];
