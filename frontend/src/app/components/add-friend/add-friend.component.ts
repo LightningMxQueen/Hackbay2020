4a418c5a6e64b0498fc2608dbc90223cbb8d9445
schw-proj <sven.schweigert@fau.de>
2020-05-27 12:38:29 +0200

    fixed add friend, started garden

--- frontend/src/app/components/add-friend/add-friend.component.ts
+++ frontend/src/app/components/add-friend/add-friend.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-friend',
@@ -10,9 +12,19 @@ export class AddFriendComponent implements OnInit {
 
   constructor(
     private userService:UsersService,
+    private bottomSheet: MatBottomSheet,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  addFriend(email:string):void{
+    this.userService.addNewFriend(email).subscribe(res => {});
+    this.bottomSheet.dismiss();
+    this.router.navigateByUrl('/home', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['/home/friends']);
+  }); 
   }
 
 }
